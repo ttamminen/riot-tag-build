2d58e618d1e905d73ba3ed79450c97223831f83a
Tatu Tamminen <ttamminen@gmail.com>
2015-04-03 12:26:52 +0300

    gulpfile updated

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,21 +2,18 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var riotify = require('riotify');
 var transform = require('vinyl-transform');
+var buffer = require('gulp-buffer');
 
 gulp.task('browserify', function () {
   // set up the browserify instance on a task basis
-  var b = browserify({
-    debug: true,
-    // defining transforms here will avoid crashing your stream
-    transform: [riotify]
-  });
+  var b = browserify({debug: true});
   // transform regular node stream to gulp (buffered vinyl) stream
   var browserified = transform(function(filename) {
     b.add(filename);
     return b.bundle();
   });
 
-  return gulp.src('./scripts/main.js')
+  return gulp.src('./main.js')
     .pipe(browserified)
     .pipe(gulp.dest('./dist/'));
 });
